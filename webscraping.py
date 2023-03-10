e97eab91d27b77e8e4c8212232255e90bfa67653
jasmine sinha <102034847+Ja3009@users.noreply.github.com>
2023-03-10 11:29:29 +0530

    Add files via upload

---
+++ webscraping.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+url = "https://codewithharry.com"
+r = requests.get(url)
+htmlContent = r.content
+soup = BeautifulSoup(htmlContent, 'html.parser')
+title = soup.title
+paras = soup.find_all('p')
+anchors = soup.find_all('a')
+all_links = set()
+for link in anchors:
+    if(link.get('href') != '#'): 
+        linkText = "https://codewithharry.com" +link.get('href')
+        all_links.add(link)
+navbarSupportedContent = soup.find(id='navbarSupportedContent')
+elem = soup.select('#loginModal')[0] 
+print(elem)
